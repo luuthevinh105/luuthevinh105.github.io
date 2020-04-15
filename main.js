37ab3bd43bf7f7127f9e932b25c4a38ff5dc4ea4
Luu The Vinh <luuthevinh1988@gmail.com>
2020-04-15 08:31:59 +0700

    portfolio

---
+++ main.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+	$('.xuong, .kn').click(function () {
+		$('body, html').animate({ scrollTop: $('#khoiedu').offset().top }, 700);
+		return false;
+	});
+
+	$('.sp').click(function () {
+		$('body, html').animate({ scrollTop: $('#khoisanpham').offset().top }, 700);
+		return false;
+	});
+});
+
+
